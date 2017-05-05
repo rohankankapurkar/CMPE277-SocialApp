45db8e8217bca5c8908abc3b96908565146e60aa
Shrutiloya <shrutiloya1@gmail.com>
2017-05-05 02:44:22 -0700

    side menu

--- src/MyApplication/app/src/main/java/com/example/rohankankapurkar/facebook/Home.java
+++ src/MyApplication/app/src/main/java/com/example/rohankankapurkar/facebook/Home.java
@@ -112,9 +112,7 @@ public class Home extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
+        }  else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
